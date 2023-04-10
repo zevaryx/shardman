d8830b67250d44190cea6fd537898d7d338b1265
zevaryx <zevaryx@gmail.com>
2023-04-10 12:12:32 -0600

    fix: Typo in Heartbeat

--- shardman/requests.py
+++ shardman/requests.py
@@ -3,7 +3,7 @@ from typing import Any
 from pydantic import BaseModel
 
 
-class Hearbeat(BaseModel):
+class Heartbeat(BaseModel):
     session_id: str
     guild_count: int | None = None
     latency: float | None = None
